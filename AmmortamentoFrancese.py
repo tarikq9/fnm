40c0df7e7cc2fd37e271a32517832bf3cc8730e4
tarikq9 <118380455+tarikq9@users.noreply.github.com>
2023-10-04 12:16:43 +0200

    Update AmmortamentoFrancese.py

--- AmmortamentoFrancese.py
+++ AmmortamentoFrancese.py
@@ -1,24 +1,28 @@
 #LEGENDA
-#n= numero di rate 
-#i= tasso di interesse 
+#n= numero rate
+#i= tasso di interesse annuo
+#k= quanti versamenti annui......inserire 12 se mensili , 6 se bimestrali , 4 trimestrali ecc...
 #S= ammontare del debito iniziale
 
-#Ammortamento francese, ovvero a rate costanti 
-def ammortamentoFrancese(n,i,S) :
+#Ammortamento francese, rate costanti 
+def ammortamentoFrancese(n,i,k,S) :
     import numpy as np
     import pandas as pd 
     from tabulate import tabulate
+    
     x=range(1,n+1)
     intervallo=np.array(x)
-    
-    Rata= np.resize(np.round(S/((1-(1+i)**-n)/i),2),n)
-    QuotaCapitale= np.round(((1+i)**-(n-intervallo+1))*Rata,2)
-    Interessi= np.round(((1-(1+i)**-(n-intervallo+1)))*Rata,2)
-    DebitoResiduo=np.round(((1-(1+i)**(-n+intervallo))/i)*Rata,2)
+    iv= (1+i)**(1/k)-1          #iv= il tasso equivalente convertito in base a k
+    Rata= np.resize(np.round(S/((1-(1+iv)**-n)/iv),2),n)
+    QuotaCapitale= np.round(((1+iv)**-(n-intervallo+1))*Rata,2)
+    Interessi= np.round(((1-(1+iv)**-(n-intervallo+1)))*Rata,2)
+    DebitoResiduo=np.round(((1-(1+iv)**(-n+intervallo))/iv)*Rata,2)
     #Creazione tabella
-    Tabella=pd.DataFrame(zip(intervallo,Rata,QuotaCapitale,Interessi,DebitoResiduo),columns=["N°Rate","Rata","Quote Capitale","Interessi","Debito Residuo"])
     
-
-    print(tabulate(Tabella,headers="keys",tablefmt="fancy_grid"))
-ammortamentoFrancese(30,0.03,100000)
-
+    tabella=pd.DataFrame(zip(intervallo,Rata,QuotaCapitale,Interessi,DebitoResiduo),columns=["N°Rate","Rata","Quote Capitale","Interessi","Debito Residuo"])
+    riga=pd.DataFrame({"N°Rate":0,"Rata":0,"Quote Capitale":0,"Interessi":0,"Debito Residuo":S},index=[0])
+    Tabella=pd.concat([riga,tabella.loc[:]]).reset_index(drop=True)
+    
+    print(tabulate(Tabella,headers="keys",tablefmt="fancy_grid",showindex="never"))
+    print("Il totale degli interessi corrisposti sarà di",round(sum(Interessi),2))
+ammortamentoFrancese(48,0.1449,12,2500)
