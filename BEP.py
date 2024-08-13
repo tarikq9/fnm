74a928347b424479d43f02dbe40a4c9edb3d9a5a
tarikq9 <118380455+tarikq9@users.noreply.github.com>
2024-08-13 15:25:19 +0200

    Update BEP.py

--- BEP.py
+++ BEP.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import math as m
 
 #Test dati
-cf=90000     #costi fissi
-cvu=200      #costi variabili unitari
-ru=500       #ricavi unitari
+cf=35000     #costi fissi
+cvu=150      #costi variabili unitari
+ru=250       #ricavi unitari
 
 def bep(tempo):
     q=cf/(ru-cvu)
@@ -17,27 +17,29 @@ def bep(tempo):
     print('il bep sarà raggiunto con un ammontare di ore pari a',ore)
     
     #Grafico  
-    x=np.linspace(0,q*2,100)     #asse x
+    fig, ax = plt.subplots()
+    x=np.linspace(0,q*2,100)     
     y1=ru*x                        #retta ricavi totali
     y2=cvu*x                       #retta costi variabili
     y3=y2+cf                       #retta costi totali
     
-    plt.hlines(y=cf,xmin=0,xmax=q*2,color="black",linestyles="-",label="costi fissi") #retta costi fissi 
-    plt.hlines(y=q*ru,xmin=0,xmax=q,color="blue",linestyle="--",alpha=0.5)
-    plt.vlines(x=q,ymin=0,ymax=q*ru,color="blue",linestyle="--",alpha=0.5)
+    plt.hlines(y=cf,xmin=0,xmax=q*2,color="darkmagenta",linestyles="-",label="costi fissi") #retta costi fissi 
+    plt.hlines(y=q*ru,xmin=0,xmax=q,color="olive",linestyle="--",alpha=0.5)
+    plt.vlines(x=q,ymin=0,ymax=q*ru,color="olive",linestyle="--",alpha=0.5)
     
-    plt.plot(x,y1,label="ricavi totali")
-    plt.plot(x,y2,label="costi variabili",color='green')
-    plt.plot(x,y3,label="costi totali",color='purple')
-    plt.fill_between(x,y1, y3,where = y1>=y3,facecolor ='green', alpha = 0.3)
-    plt.fill_between(x,y1,y3,where= y1<=y3,facecolor = "red",alpha=0.3)
-    plt.plot(q,q*ru,"bo",label="break even point")
+    
+    plt.plot(x,y2,label="costi variabili",color='darkorange')
+    plt.plot(x,y3,label="costi totali",color='crimson')
+    plt.plot(x,y1,label="ricavi totali",color='darkblue')
+    plt.fill_between(x,y1, y3,where = y1>=y3,facecolor ='seagreen', alpha = 0.25)
+    plt.fill_between(x,y1,y3,where= y1<=y3,facecolor = "firebrick",alpha=0.3)
+    plt.plot(q,q*ru,"yo",label="break even point")
     
     plt.xlabel("QUANTITA DA VENDERE")
     plt.ylabel("COSTI E RICAVI")
-    plt.grid(linestyle='--',linewidth=0.5)
     plt.margins(0)
     plt.legend()
     plt.title("BREAK EVEN POINT")
+    ax.set_facecolor('floralwhite')
     plt.show()   
-bep(32/60) #tempo medio per produrre 1 unità, espresso in ore
+bep(30/60) #tempo medio per produrre 1 unità, espresso in ore
